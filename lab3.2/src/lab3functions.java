564660f2f8403f50eb82134d6ce6bc7723baefef
Derek <dyu5074@bths.edu>
2017-11-21 00:50:45 -0500

    finished merge , started mergesort

--- lab3.2/src/lab3functions.java
+++ lab3.2/src/lab3functions.java
@@ -2,8 +2,8 @@
 public class lab3functions {
 	public static void main(String[]args)
 	{
-		String [] string1 = {"a","c","e"};
-		String [] string2 = {"b","d","f"};
+		String [] string1 = {"a","b","c"};
+		String [] string2 = {"d","e","f"};
 		printArray(merge(string1,string2));
 	}
 	public static String[] merge(String[] list1, String[] list2)
@@ -12,31 +12,29 @@ public class lab3functions {
 		int newlist2 = 0;
 		int newlist3 = 0;
 		String[] newarray = new String[list1.length + list2.length];
-		while(newlist < list1.length && newlist2 < list2.length)
+		while(newlist < list1.length || newlist2 < list2.length)
 		{
-			if (list1[newlist].compareTo(list2[newlist2]) > 0)
+			if (newlist == list1.length)
 			{
 				newarray[newlist3] = list2[newlist2];
-				newlist3++;
 				newlist2++;
+			}
+			else if (newlist2 == list2.length)
+			{
+				newarray[newlist3] = list1[newlist];
+				newlist++;
 			}
 			else if (list1[newlist].compareTo(list2[newlist2]) < 0)
 			{
 				newarray[newlist3] = list1[newlist];
-				newlist3++;
 				newlist++;
 			}
 			else 
 			{
-				newarray[newlist3] = list1[newlist];
-				newlist++;
-				newlist3++;
+				newarray[newlist3] = list2[newlist2];
 				newlist2++;
 			}
-		}
-		for (int i = 0; i < list1.length && i < list2.length;i++)
-		{
-			
+			newlist3++;
 		}
 		return newarray;
 	}
@@ -50,7 +48,13 @@ public class lab3functions {
 	}
 	public static String[] mergeSort(String[] list)
 	{
-		
+		if (list.length <= 1) {
+			return list;
+		}
+		else
+		{
+			
+		}
 	}
 	public static int partition(int[] list)
 	{
